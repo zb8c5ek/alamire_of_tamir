01f6752028e05f36fa3702185b3439ab0d622200
xuchen <flora1011>
2016-03-25 16:18:41 +0100

    Annocement_added

--- alamire_of_tamir/__init__.py
+++ alamire_of_tamir/__init__.py
@@ -1,5 +1,8 @@
+"""
+    Contact Information of author is REMOVED due to double blind Review. They will be added back after review session!
+    Thank you for your attention and patience!
+"""
 from script_translation import script_transcription
-
 
 def transcribe_script(annotation_filename, output_filename=None):
     print "This is version 0.12"
