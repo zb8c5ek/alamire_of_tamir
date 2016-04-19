c4c5a09b83adfa75a51da7c27ccf2c2515b0ee62
xuchen <flora1011>
2016-04-19 15:34:48 +0200

    License File updated. Demo Files added to: /your/install/site-package/path/alamire_of_tamir/data_alamire/demo.annotation

--- alamire_of_tamir/__init__.py
+++ alamire_of_tamir/__init__.py
@@ -5,5 +5,4 @@
 from script_translation import script_transcription
 
 def transcribe_script(annotation_filename, output_filename=None):
-    print "This is version 0.12"
     script_transcription(annotation_filename, output_filename)
--- setup.py
+++ setup.py
@@ -1,26 +1,28 @@
 from setuptools import setup
 
 setup(name='alamire_of_tamir',
-      version='0.12',
+      version='0.15.1',
       description='Translate the manuscripts',
-      # long_description=readme(),
-      # classifiers=[
-      #     'License :: OSI Approved :: GNU License',
-      #     'Programming Language :: Python :: 2.7',
-      #     'Topic :: Computer Vision :: Manuscript Transcription',
-      # ],
+      url='https://github.com/zb8c5ek/alamire_of_tamir',
       keywords='Manuscript Transcription, Alamire, TAMIR',
-      url='https://github.com/zb8c5ek/alamire_of_tamir.git',
-      author='Xuanli Chen',
-      author_email='xuanli.chen@esat.kuleuven.be',
-      license='GNU',
+
+      author='RemovedAuthorInformationDueToDoubleBlindReview',
+      author_email='RemovedAuthorContactDueToDoubleBlindReview',
+      license='MIT',
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+
+      ],
       packages=['alamire_of_tamir'],
       install_requires=[
           'music21', 'numpy'
       ],
       test_suite='nose.collector',
       tests_require=['nose'],
-          # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
       entry_points={
           'console_scripts': ['alamire_translation=alamire_of_tamir.command_line:main'],
       },
