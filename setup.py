2a0cf39b33003c06293a894b47294d1c7885c685
zb8c5ek <chenxuanli.alphonse@gmail.com>
2016-07-19 18:59:21 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
 from setuptools import setup
-
+"""
+Xuanli Chen
+PhD student at PSI-VISICS, KU Leuven
+Supervisor: Prof. Luc Van Gool
+Research Domain: Computer Vision, Machine Learning
+Address:
+Kasteelpark Arenberg 10 - bus 2441
+B-3001 Heverlee
+Belgium
+Group website: http://www.esat.kuleuven.be/psi/visics
+LinkedIn: https://be.linkedin.com/in/xuanlichen
+"""
 setup(name='alamire_of_tamir',
       version='0.15.1',
       description='Translate the manuscripts',
