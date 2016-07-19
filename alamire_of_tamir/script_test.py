b4d4e2a1f7756a5c5cc79559741e908e45109d25
zb8c5ek <chenxuanli.alphonse@gmail.com>
2016-07-19 18:58:51 +0200

    Update script_test.py

--- alamire_of_tamir/script_test.py
+++ alamire_of_tamir/script_test.py
@@ -1,5 +1,16 @@
 import music21 as m21
-
+"""
+Xuanli Chen
+PhD student at PSI-VISICS, KU Leuven
+Supervisor: Prof. Luc Van Gool
+Research Domain: Computer Vision, Machine Learning
+Address:
+Kasteelpark Arenberg 10 - bus 2441
+B-3001 Heverlee
+Belgium
+Group website: http://www.esat.kuleuven.be/psi/visics
+LinkedIn: https://be.linkedin.com/in/xuanlichen
+"""
 s = m21.stream.Stream()
 clef = m21.medren.MensuralClef(sign='C')
 # ts = m21.medren.Mensuration(tempus='imperfect', prolation='minor', mode='imperfect', scalingFactor=2)
