1d4f4fcaa31f994e3c16e826efe7e6fd6b77c65c
zb8c5ek <chenxuanli.alphonse@gmail.com>
2016-07-19 18:59:03 +0200

    Update script_translation.py

--- alamire_of_tamir/script_translation.py
+++ alamire_of_tamir/script_translation.py
@@ -1,7 +1,17 @@
 import os
-
 from read_write_init import *
-
+"""
+Xuanli Chen
+PhD student at PSI-VISICS, KU Leuven
+Supervisor: Prof. Luc Van Gool
+Research Domain: Computer Vision, Machine Learning
+Address:
+Kasteelpark Arenberg 10 - bus 2441
+B-3001 Heverlee
+Belgium
+Group website: http://www.esat.kuleuven.be/psi/visics
+LinkedIn: https://be.linkedin.com/in/xuanlichen
+"""
 
 def script_transcription(
         annotation_file='/users/visics/xchen/project_Alamire/alamire/dots_test.annotation',
