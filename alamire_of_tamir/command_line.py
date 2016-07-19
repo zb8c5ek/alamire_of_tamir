b1ce2582017a683604fb7c33a5bfdd7ee29b1c3b
zb8c5ek <chenxuanli.alphonse@gmail.com>
2016-07-19 18:58:03 +0200

    Update command_line.py

--- alamire_of_tamir/command_line.py
+++ alamire_of_tamir/command_line.py
@@ -1,5 +1,16 @@
 from . import transcribe_script
-
+"""
+Xuanli Chen
+PhD student at PSI-VISICS, KU Leuven
+Supervisor: Prof. Luc Van Gool
+Research Domain: Computer Vision, Machine Learning
+Address:
+Kasteelpark Arenberg 10 - bus 2441
+B-3001 Heverlee
+Belgium
+Group website: http://www.esat.kuleuven.be/psi/visics
+LinkedIn: https://be.linkedin.com/in/xuanlichen
+"""
 
 def main(annotation_filename, output_filename=None):
     transcribe_script(annotation_filename=annotation_filename, output_filename=output_filename)
